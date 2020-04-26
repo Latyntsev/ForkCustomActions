7f9dd369919dd2dd4a2246c6da3533b9702abe3e
Aleksandr Latyntsev <latyntsev.a@gmail.com>
2020-04-26 12:16:47 +0300

    update

--- github_PR_tajawal.sh
+++ github_PR_tajawal.sh
@@ -3,10 +3,9 @@ source ~/.bash_profile
 git push
 
 branch=$(git rev-parse --abbrev-ref HEAD)
-
+response=$(curl -s "${tajawal_jira_url}/rest/api/2/issue/$branch" -u "$tajawal_jira_access_token" | sed 's#\\n##g;s#\\#\\\\#g')
 base_branch=$1
 
-response=$(curl -s "${tajawal_jira_url}/rest/api/2/issue/$branch" -u "$tajawal_jira_access_token" | sed 's#\\n##g;s#\\#\\\\#g')
 
 if [ -z "$base_branch" ]; then
 	base_branch=$(git show-branch | grep '*' | grep -v "$(git rev-parse --abbrev-ref HEAD)" | head -n1 | sed 's/.*\[\(.*\)\].*/\1/' | sed 's/[\^~].*//')
