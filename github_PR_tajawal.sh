127e4c38f782e25df9aecc092d78c53fbc024d15
Mohammad Alatrash <mkalatrash@gmail.com>
2020-04-26 13:04:55 +0400

    refactoring

--- github_PR_tajawal.sh
+++ github_PR_tajawal.sh
@@ -4,11 +4,7 @@ git push
 
 branch=$(git rev-parse --abbrev-ref HEAD)
 
-base_branch=$(git show-branch -a 2>/dev/null \
-| grep '\*' \
-| grep -v `git rev-parse --abbrev-ref HEAD` \
-| head -n1 \
-| perl -ple 's/\[[A-Za-z]+-\d+\][^\]]+$//; s/^.*\[([^~^\]]+).*$/$1/')
+base_branch=$(git show-branch | grep '*' | grep -v "$(git rev-parse --abbrev-ref HEAD)" | head -n1 | sed 's/.*\[\(.*\)\].*/\1/' | sed 's/[\^~].*//')
 
 response=$(curl -s "${tajawal_jira_url}/rest/api/2/issue/$branch" -u "$tajawal_jira_access_token" | sed 's#\\n##g;s#\\#\\\\#g')
 
